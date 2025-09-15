a61a8c4ee7b49461df1d031eb3f239da20452650
jaydenlu67 <jaydenlu1967@gmail.com>
2025-09-15 10:51:39 -0400

    created a python file to be creepy...

---
+++ p1.py
@@ -0,0 +1,8 @@
+# My first program
+# Created on 09/15/2025
+# Purpose: to learn python
+# Editor: Jayden
+
+name = input("Hey there, can I have your name?: ")
+age = input("How old are you?: ")
+print("Hey there, " + name + " woah you so cool you are " + age + " years old!!")
